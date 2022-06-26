2ce199e4b610a3123d989ec8dad123c9f91d802f
dantmnf <dantmnf@users.noreply.github.com>
2022-06-27 01:19:43 +0800

    use numpy 1.21.3 (first version that supports 3.10)

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd onnxruntime
-./build.sh --update --build --skip_submodule_sync --build_shared_lib --enable_pybind --build_wheel --config Release --parallel
+./build.sh --update --build --skip_submodule_sync --build_shared_lib --enable_pybind --build_wheel --numpy_version 1.21.3 --config Release --parallel
 mkdir -p ../wheels-linux
 shopt -s globstar
 cp -v build/**/dist/*.whl ../wheels-linux
